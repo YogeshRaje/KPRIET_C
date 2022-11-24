ace0729a6dc53dab988dccc0ed721f431e403b71
Yogesh Raje <yhraje@gmail.com>
2022-11-24 21:57:17 +0530

    Add files via upload

---
+++ Day4 programs c++/1a_inputcin.cpp
@@ -0,0 +1,12 @@
+//input 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;   // Taking input
+    cout << "The number is: " << num;
+    return 0;
+}
+
---
+++ Day4 programs c++/1cout.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // prints the string enclosed in double quotes
+    cout << "Hello Student ,this is C++ Programming";
+    return 0;
+}
+
